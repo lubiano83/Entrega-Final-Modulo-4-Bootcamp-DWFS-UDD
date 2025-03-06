1c0c36734ed60bac9a21ab23db37ab125ed52ea7
lubiano83 <josepablolubiano@lubiano83.local>
2025-03-06 17:02:44 -0300

    agregando swagger

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 import exprress from "express";
 import lodgesRouter from "./routes/lodges.router.js";
 import reservationsRouter from "./routes/reservations.router.js";
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUiExpress from "swagger-ui-express";
 
 // Variables
 const APP = exprress();
 const PORT = 8080;
 const HOST = "localhost";
+
+// Swagger
+const swaggerOptions = {
+    definition: {
+        openapi: "3.0.1",
+        info: {
+            title: "Documentación de la App de Reservas de Cabañas",
+            description: "App dedicada a la administración y reserva de cabañas para hospedaje."
+        }
+    },
+    apis: ["./src/docs/**/*.yaml"] // Esto lee todos las carpetas y sus archivos .yaml dentro de docs.
+}
+
+const specs = swaggerJSDoc(swaggerOptions);
 
 // Middlewares
 APP.use(exprress.json());
@@ -14,6 +30,7 @@ APP.use(exprress.urlencoded({ extended: true }));
 // Routes
 APP.use("/api/lodges", lodgesRouter);
 APP.use("/api/reservations", reservationsRouter);
+APP.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs));
 
 // Método que gestiona las rutas inexistentes.
 APP.use("*", (req, res) => {
@@ -26,5 +43,5 @@ APP.use((error, req, res) => {
     res.status(500).send("<h1>Error 500: Error en el Servidor</h1>");
 });
 
-// Listen
+// Listening
 APP.listen(PORT, () => console.log(`Escuchando en http://${HOST}:${PORT}`));
