5477ec641f99899a28ab47f6ad628aa7b951efe3
lubiano83 <josepablolubiano@MacBook-Air-de-Jose.local>
2025-03-13 01:33:21 -0300

    agregando validaciones en reservas

--- src/routes/reservations.router.js
+++ src/routes/reservations.router.js
@@ -8,8 +8,9 @@ ROUTER.get("/", reservationsController.getReservations);
 ROUTER.post("/", reservationsController.createReservation);
 ROUTER.delete("/", reservationsController.deleteAllReservations);
 ROUTER.get("/:id", reservationsController.getReservationById);
-ROUTER.put("/:id", reservationsController.updateReservationById);
 ROUTER.delete("/:id", reservationsController.deleteReservationById);
 ROUTER.patch("/:id", reservationsController.isAlreadyPaid);
+ROUTER.put("/user/:id", reservationsController.updateReservationUserById);
+ROUTER.put("/lodge/:id", reservationsController.updateReservationLodgeById);
 
 export default ROUTER;
